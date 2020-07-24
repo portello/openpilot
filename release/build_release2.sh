3067279ffc69e9bd04e3606a9f54a815deee7740
Jason Young <jyoung8607@gmail.com>
2020-07-24 08:45:32 -0400

    openpilot v0.7.8 release

--- release/build_release2.sh
+++ release/build_release2.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-export GIT_COMMITTER_NAME="Vehicle Researcher"
-export GIT_COMMITTER_EMAIL="user@comma.ai"
-export GIT_AUTHOR_NAME="Vehicle Researcher"
-export GIT_AUTHOR_EMAIL="user@comma.ai"
-
+export GITHUB_REPO="jyoung8607/openpilot.git"
+export GIT_COMMITTER_NAME="Jason Young"
+export GIT_COMMITTER_EMAIL="jyoung8607@gmail.com"
+export GIT_AUTHOR_NAME="Jason Young"
+export GIT_AUTHOR_EMAIL="jyoung8607@gmail.com"
 export GIT_SSH_COMMAND="ssh -i /data/gitkey"
 
 # set CLEAN to build outside of CI
@@ -17,7 +17,7 @@ if [ ! -z "$CLEAN" ]; then
 
   # Create git repo
   git init
-  git remote add origin git@github.com:commaai/openpilot.git
+  git remote add origin git@github.com:$GITHUB_REPO
   git fetch origin devel-staging
 else
   cd /data/openpilot
@@ -26,7 +26,6 @@ else
 fi
 
 git fetch origin release2-staging
-git fetch origin dashcam-staging
 
 # Create release2 with no history
 if [ ! -z "$CLEAN" ]; then
@@ -38,15 +37,8 @@ fi
 VERSION=$(cat selfdrive/common/version.h | awk -F\" '{print $2}')
 git commit -m "openpilot v$VERSION"
 
-# Build signed panda firmware
-pushd panda/board/
-cp -r /tmp/pandaextra /data/openpilot/
-RELEASE=1 make obj/panda.bin
-mv obj/panda.bin /tmp/panda.bin
-make clean
-mv /tmp/panda.bin obj/panda.bin.signed
-rm -rf /data/openpilot/pandaextra
-popd
+# Panda firmware would ordinarily build here, but only Comma can make release-signed builds.
+rm -f panda/board/obj/panda.bin.signed
 
 # Build stuff
 ln -sfn /data/openpilot /data/pythonpath
@@ -79,14 +71,8 @@ git commit --amend -m "openpilot v$VERSION"
 #git status --ignored
 
 if [ ! -z "$PUSH" ]; then
-  git remote set-url origin git@github.com:commaai/openpilot.git
+  git remote set-url origin git@github.com:$GITHUB_REPO
 
   # Push to release2-staging
   git push -f origin release2-staging
-
-  # Create dashcam release
-  git rm selfdrive/car/*/carcontroller.py
-
-  git commit -m "create dashcam release from release2"
-  git push -f origin release2-staging:dashcam-staging
 fi
